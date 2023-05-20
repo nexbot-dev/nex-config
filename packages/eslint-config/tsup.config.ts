f907057312fb21e4c97075affac43f5c39591f2b
Aen Jojo <josuafernando999@gmail.com>
2023-05-20 15:09:25 +0700

    style: fix spacing

--- packages/eslint-config/tsup.config.ts
+++ packages/eslint-config/tsup.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  splitting: false,
-  clean: true,
-  target: 'es2021',
-  keepNames: true,
-  treeshake: true,
-  skipNodeModulesBundle: true,
-  format: ['cjs'],
-  minify: false,
+	entry: ['src/index.ts'],
+	splitting: false,
+	clean: true,
+	target: 'es2021',
+	keepNames: true,
+	treeshake: true,
+	skipNodeModulesBundle: true,
+	format: ['cjs'],
+	minify: false,
 });
